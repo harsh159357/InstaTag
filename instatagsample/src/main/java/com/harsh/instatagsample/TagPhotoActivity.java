8d5a00329e16248f8fbf3893e4653f2f510b0a01
HARSH SHARMA <harsh159357@gmail.com>
2017-05-23 23:51:07 +0530

    Change in App Main Activity and Loading of photo in imageview from URI using glide

--- instatagsample/src/main/java/com/harsh/instatagsample/TagPhotoActivity.java
+++ instatagsample/src/main/java/com/harsh/instatagsample/TagPhotoActivity.java
@@ -4,7 +4,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.harsh.instatag.SquareImageView;
+
+import java.io.File;
 //import com.squareup.picasso.Picasso;
 
 public class TagPhotoActivity extends AppCompatActivity {
@@ -18,6 +22,13 @@ public class TagPhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tag_photo);
         photoToBeTaggedUri = getIntent().getData();
         photoToBeTaggedImageView = (SquareImageView) findViewById(R.id.photo_to_be_tagged);
+        Glide.with(this).load(photoToBeTaggedUri)
+                .centerCrop()
+                .placeholder(0)
+                .fallback(0)
+                .skipMemoryCache(false)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(photoToBeTaggedImageView);
 //        Picasso.with(this).load(photoToBeTaggedUri).into(photoToBeTaggedImageView);
     }
 }
