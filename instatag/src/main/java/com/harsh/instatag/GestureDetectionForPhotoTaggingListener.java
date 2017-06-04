88cac91976fae56e14f0ce6a5f37622c3cde84db
HARSH SHARMA <harsh159357@gmail.com>
2017-06-04 23:05:19 +0530

    Change of GestureDetection class to a interface

--- instatag/src/main/java/com/harsh/instatag/GestureDetectionForPhotoTaggingListener.java
+++ instatag/src/main/java/com/harsh/instatag/GestureDetectionForPhotoTaggingListener.java
@@ -1,87 +1,27 @@
 package com.harsh.instatag;
 
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 
-public class GestureDetectionForPhotoTaggingListener implements GestureDetector.OnGestureListener,
+public interface GestureDetectionForPhotoTaggingListener extends GestureDetector.OnGestureListener,
         GestureDetector.OnDoubleTapListener {
 
+    boolean onDown(MotionEvent e);
 
-    @Override
-    public boolean onDown(MotionEvent e) {
-        Log.d("Gesture ", " onDown");
-        return true;
-    }
+    boolean onSingleTapConfirmed(MotionEvent e);
 
+    boolean onSingleTapUp(MotionEvent e);
 
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        Log.d("Gesture ", " onSingleTapConfirmed");
-        return true;
-    }
+    void onShowPress(MotionEvent e);
 
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        Log.d("Gesture ", " onSingleTapUp");
-        return true;
-    }
+    boolean onDoubleTap(MotionEvent e);
 
-    @Override
-    public void onShowPress(MotionEvent e) {
-        Log.d("Gesture ", " onShowPress");
-    }
+    boolean onDoubleTapEvent(MotionEvent e);
 
-    @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        Log.d("Gesture ", " onDoubleTap");
-        return true;
-    }
+    void onLongPress(MotionEvent e);
 
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        Log.d("Gesture ", " onDoubleTapEvent");
-        return true;
-    }
+    boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY);
 
-    @Override
-    public void onLongPress(MotionEvent e) {
-        Log.d("Gesture ", " onLongPress");
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-
-        Log.d("Gesture ", " onScroll");
-        if (e1.getY() < e2.getY()) {
-            Log.d("Gesture ", " Scroll Down");
-        }
-        if (e1.getY() > e2.getY()) {
-            Log.d("Gesture ", " Scroll Up");
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        if (e1.getX() < e2.getX()) {
-            Log.d("Gesture ", "Left to Right swipe: " + e1.getX() + " - " + e2.getX());
-            Log.d("Speed ", String.valueOf(velocityX) + " pixels/second");
-        }
-        if (e1.getX() > e2.getX()) {
-            Log.d("Gesture ", "Right to Left swipe: " + e1.getX() + " - " + e2.getX());
-            Log.d("Speed ", String.valueOf(velocityX) + " pixels/second");
-        }
-        if (e1.getY() < e2.getY()) {
-            Log.d("Gesture ", "Up to Down swipe: " + e1.getX() + " - " + e2.getX());
-            Log.d("Speed ", String.valueOf(velocityY) + " pixels/second");
-        }
-        if (e1.getY() > e2.getY()) {
-            Log.d("Gesture ", "Down to Up swipe: " + e1.getX() + " - " + e2.getX());
-            Log.d("Speed ", String.valueOf(velocityY) + " pixels/second");
-        }
-        return true;
-
-    }
+    boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY);
 }
 
