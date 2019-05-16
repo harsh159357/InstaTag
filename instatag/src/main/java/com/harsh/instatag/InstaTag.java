44844c0cc06d66bd70d21af51afb0462fa29858b
HARSH SHARMA <harsh159357@gmail.com>
2019-05-16 23:21:25 +0530

    Tasks Done -
    1. Gradle Version Update.
    2. Left, Right and Bottom Carrot Removal from the App.
    3. Redundant Resource or unused resources are removed now.

--- instatag/src/main/java/com/harsh/instatag/InstaTag.java
+++ instatag/src/main/java/com/harsh/instatag/InstaTag.java
@@ -62,19 +62,12 @@ public class InstaTag extends RelativeLayout {
     private int mTagTextColor;
     private int mTagBackgroundColor;
     private int mCarrotTopBackGroundColor;
-    private int mCarrotLeftBackGroundColor;
-    private int mCarrotRightBackGroundColor;
-    private int mCarrotBottomBackGroundColor;
 
     private Drawable mTagTextDrawable;
     private Drawable mCarrotTopDrawable;
-    private Drawable mCarrotLeftDrawable;
-    private Drawable mCarrotRightDrawable;
-    private Drawable mCarrotBottomDrawable;
 
     private boolean tagsAreAdded;
     private boolean canWeAddTags;
-    private boolean showAllCarrots;
     private boolean mIsRootIsInTouch = true;
 
     private Animation mShowAnimation;
@@ -180,11 +173,6 @@ public class InstaTag extends RelativeLayout {
     public interface Constants {
         int DEFAULT_COLOR = 0xFF303F9F;
         int TAG_TEXT_COLOR = Color.WHITE;
-
-        String CARROT_TOP = "CARROT_TOP";
-        String CARROT_LEFT = "CARROT_LEFT";
-        String CARROT_RIGHT = "CARROT_RIGHT";
-        String CARROT_BOTTOM = "CARROT_BOTTOM";
     }
 
     public interface PhotoEvent {
@@ -260,17 +248,9 @@ public class InstaTag extends RelativeLayout {
                 getDrawable(R.styleable.InstaTag_tagTextBackground);
         mCarrotTopDrawable = obtainStyledAttributes.
                 getDrawable(R.styleable.InstaTag_carrotTopBackground);
-        mCarrotLeftDrawable = obtainStyledAttributes.
-                getDrawable(R.styleable.InstaTag_carrotLeftBackground);
-        mCarrotRightDrawable = obtainStyledAttributes.
-                getDrawable(R.styleable.InstaTag_carrotRightBackground);
-        mCarrotBottomDrawable = obtainStyledAttributes.
-                getDrawable(R.styleable.InstaTag_carrotBottomBackground);
 
         canWeAddTags = obtainStyledAttributes.
                 getBoolean(R.styleable.InstaTag_canWeAddTags, false);
-        showAllCarrots = obtainStyledAttributes.
-                getBoolean(R.styleable.InstaTag_showAllCarrots, false);
 
         mTagTextColor = obtainStyledAttributes.
                 getColor(R.styleable.InstaTag_instaTextColor, Constants.TAG_TEXT_COLOR);
@@ -281,20 +261,11 @@ public class InstaTag extends RelativeLayout {
         if (overrideDefaultColor == Constants.DEFAULT_COLOR) {
             mCarrotTopBackGroundColor = obtainStyledAttributes.
                     getColor(R.styleable.InstaTag_carrotTopColor, Constants.DEFAULT_COLOR);
-            mCarrotLeftBackGroundColor = obtainStyledAttributes.
-                    getColor(R.styleable.InstaTag_carrotLeftColor, Constants.DEFAULT_COLOR);
-            mCarrotRightBackGroundColor = obtainStyledAttributes.
-                    getColor(R.styleable.InstaTag_carrotRightColor, Constants.DEFAULT_COLOR);
-            mCarrotBottomBackGroundColor = obtainStyledAttributes.
-                    getColor(R.styleable.InstaTag_carrotBottomColor, Constants.DEFAULT_COLOR);
             mTagBackgroundColor = obtainStyledAttributes.
                     getColor(R.styleable.InstaTag_instaBackgroundColor, Constants.DEFAULT_COLOR);
         } else {
             mTagBackgroundColor = overrideDefaultColor;
             mCarrotTopBackGroundColor = overrideDefaultColor;
-            mCarrotLeftBackGroundColor = overrideDefaultColor;
-            mCarrotRightBackGroundColor = overrideDefaultColor;
-            mCarrotBottomBackGroundColor = overrideDefaultColor;
         }
 
         mHideAnimation = AnimationUtils.loadAnimation(context, obtainStyledAttributes.
@@ -307,59 +278,12 @@ public class InstaTag extends RelativeLayout {
         obtainStyledAttributes.recycle();
     }
 
-    private void setCarrotVisibility(View tagView, String carrotType) {
-        if (!showAllCarrots) {
-            switch (carrotType) {
-                case Constants.CARROT_TOP:
-                    tagView.findViewById(R.id.carrot_top).setVisibility(View.VISIBLE);
-
-                    tagView.findViewById(R.id.carrot_left).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_right).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_bottom).setVisibility(View.INVISIBLE);
-                    break;
-                case Constants.CARROT_LEFT:
-                    tagView.findViewById(R.id.carrot_left).setVisibility(View.VISIBLE);
-
-                    tagView.findViewById(R.id.carrot_top).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_right).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_bottom).setVisibility(View.INVISIBLE);
-                    break;
-                case Constants.CARROT_RIGHT:
-                    tagView.findViewById(R.id.carrot_right).setVisibility(View.VISIBLE);
-
-                    tagView.findViewById(R.id.carrot_top).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_left).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_bottom).setVisibility(View.INVISIBLE);
-                    break;
-                case Constants.CARROT_BOTTOM:
-                    tagView.findViewById(R.id.carrot_bottom).setVisibility(View.VISIBLE);
-
-                    tagView.findViewById(R.id.carrot_top).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_left).setVisibility(View.INVISIBLE);
-                    tagView.findViewById(R.id.carrot_right).setVisibility(View.INVISIBLE);
-                    break;
-            }
-        }
-    }
-
     private void setColorForTag(View tagView) {
         ((TextView) tagView.findViewById(R.id.tag_text_view)).setTextColor(mTagTextColor);
 
         if (mCarrotTopDrawable == null) {
             setColor((tagView.findViewById(R.id.carrot_top)).
                     getBackground(), mCarrotTopBackGroundColor);
-        }
-        if (mCarrotLeftDrawable == null) {
-            setColor((tagView.findViewById(R.id.carrot_left)).
-                    getBackground(), mCarrotLeftBackGroundColor);
-        }
-        if (mCarrotRightDrawable == null) {
-            setColor((tagView.findViewById(R.id.carrot_right)).
-                    getBackground(), mCarrotRightBackGroundColor);
-        }
-        if (mCarrotBottomDrawable == null) {
-            setColor((tagView.findViewById(R.id.carrot_bottom)).
-                    getBackground(), mCarrotBottomBackGroundColor);
         }
 
         if (mTagTextDrawable == null) {
@@ -446,12 +370,6 @@ public class InstaTag extends RelativeLayout {
 
             final LinearLayout carrotTopContainer =
                     tagView.findViewById(R.id.carrot_top);
-            final LinearLayout carrotLeftContainer =
-                    tagView.findViewById(R.id.carrot_left);
-            final LinearLayout carrotRightContainer =
-                    tagView.findViewById(R.id.carrot_right);
-            final LinearLayout carrotBottomContainer =
-                    tagView.findViewById(R.id.carrot_bottom);
 
             final ImageView removeTagImageView =
                     tagView.findViewById(R.id.remove_tag_image_view);
@@ -463,22 +381,6 @@ public class InstaTag extends RelativeLayout {
             }
             if (mCarrotTopDrawable != null) {
                 ViewCompat.setBackground(carrotTopContainer, mCarrotTopDrawable);
-            }
-            if (mCarrotLeftDrawable != null) {
-                ViewCompat.setBackground(carrotLeftContainer, mCarrotLeftDrawable);
-            }
-            if (mCarrotRightDrawable != null) {
-                ViewCompat.setBackground(carrotRightContainer, mCarrotRightDrawable);
-            }
-            if (mCarrotBottomDrawable != null) {
-                ViewCompat.setBackground(carrotBottomContainer, mCarrotBottomDrawable);
-            }
-
-            if (showAllCarrots) {
-                tagView.findViewById(R.id.carrot_top).setVisibility(View.VISIBLE);
-                tagView.findViewById(R.id.carrot_left).setVisibility(View.VISIBLE);
-                tagView.findViewById(R.id.carrot_right).setVisibility(View.VISIBLE);
-                tagView.findViewById(R.id.carrot_bottom).setVisibility(View.VISIBLE);
             }
 
             tagTextView.setText(tagText);
@@ -520,7 +422,8 @@ public class InstaTag extends RelativeLayout {
                 public void onDragEnd(View view) {
                 }
             }));
-            tagView.setOnTouchListener(tagOnTouchListener);
+            if (canWeAddTags)
+                tagView.setOnTouchListener(tagOnTouchListener);
         } else {
             Toast.makeText(mContext, "This user is already tagged", Toast.LENGTH_SHORT).show();
         }
@@ -667,18 +570,6 @@ public class InstaTag extends RelativeLayout {
         return mCarrotTopBackGroundColor;
     }
 
-    public int getCarrotLeftBackGroundColor() {
-        return mCarrotLeftBackGroundColor;
-    }
-
-    public int getCarrotRightBackGroundColor() {
-        return mCarrotRightBackGroundColor;
-    }
-
-    public int getCarrotBottomBackGroundColor() {
-        return mCarrotBottomBackGroundColor;
-    }
-
     public Drawable getTagTextDrawable() {
         return mTagTextDrawable;
     }
@@ -687,28 +578,12 @@ public class InstaTag extends RelativeLayout {
         return mCarrotTopDrawable;
     }
 
-    public Drawable getCarrotLeftDrawable() {
-        return mCarrotLeftDrawable;
-    }
-
-    public Drawable getCarrotRightDrawable() {
-        return mCarrotRightDrawable;
-    }
-
-    public Drawable getCarrotBottomDrawable() {
-        return mCarrotBottomDrawable;
-    }
-
     public boolean canWeAddTags() {
         return canWeAddTags;
     }
 
     public void setCanWeAddTags(boolean mCanWeAddTags) {
         this.canWeAddTags = mCanWeAddTags;
-    }
-
-    public boolean isShowAllCarrots() {
-        return showAllCarrots;
     }
 
     public void setLikeResource(@DrawableRes int resource) {
